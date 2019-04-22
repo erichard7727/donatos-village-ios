9eb63f382c0a71753760ea4d1bbd6caed816af39
Rob Feldmann <me@robfeldmann.com>
2019-04-22 12:22:22 -0400

    DONV-34 Update SteamService

--- Product/Core/VillageCore/Source/Services/StreamsService.swift
+++ Product/Core/VillageCore/Source/Services/StreamsService.swift
@@ -12,9 +12,33 @@ import SwiftyJSON
 
 enum StreamsServiceError: Error {
     case unknown
-    case streamDetails
-    case sendMessage
-    case subscribeToStream
+}
+
+fileprivate extension Stream.StreamType {
+    
+    var apiType: VillageCoreAPI.StreamType {
+        switch self {
+        case .open: return .open
+        case .memberInvites: return .memberInvites
+        case .closed: return .closed
+        case .global: return .global
+        }
+    }
+    
+    init?(apiValue: String?) {
+        switch apiValue?.lowercased() {
+        case "open"?:
+            self = .open
+        case "open_invite"?:
+            self = .memberInvites
+        case "closed"?:
+            self = .closed
+        case "global"?:
+            self = .global
+        default:
+            return nil
+        }
+    }
 }
 
 struct StreamsService {
@@ -36,33 +60,19 @@ struct StreamsService {
         }
     }
     
-    //TODO: Ask harry styles about this thing
-//    static func inviteUsersToStream(streamInvite: StreamInvite) -> Promise<StreamInvite> {
-//        return firstly {
-//            let invite = VillageCoreAPI.inviteUsersToStream(streamInvite: streamInvite)
-//            return VillageService.shared.request(target: invite)
-//        }.then { (json: JSON) -> StreamInvite in
-//            let streamInvites = StreamInvite(from: json)
-//            return streamInvites
-//        }
-//    }
+    static func getHomeStream(page: Int) -> Promise<Void> {
+        #warning("TODO - Implement home stream")
+        return Promise(Void())
+    }
     
-    static func getStreamMembers(streamId: String) -> Promise<People> {
+    static func subscribedStreams() -> Promise<Streams> {
         return firstly {
-            let members = VillageCoreAPI.streamMembers(streamId: streamId)
-            return VillageService.shared.request(target: members)
-        }.then { (json: JSON) -> People in
-            let people = json["people"].arrayValue.compactMap({ Person(from: $0) })
-            return people
+            let subscriptions = VillageCoreAPI.subscribedStreams
+            return VillageService.shared.request(target: subscriptions)
+        }.then { (json: JSON) -> Streams in
+            let streams = json["streams"].arrayValue.compactMap({ Stream(from: $0) })
+            return streams
         }
-    }
-    
-    static func likeMessage(streamId: String, messageId: String) -> Promise<Void> {
-        return VillageService.shared.request(target: VillageCoreAPI.setMessageLiked(isLiked: true, messageId: messageId, streamId: streamId)).asVoid()
-    }
-    
-    static func dislikeMessage(streamId: String, messageId: String) -> Promise<Void> {
-        return VillageService.shared.request(target: VillageCoreAPI.setMessageLiked(isLiked: false, messageId: messageId, streamId: streamId)).asVoid()
     }
     
     static func getOtherStreams(page: Int) -> Promise<Streams> {
@@ -70,55 +80,132 @@ struct StreamsService {
             let streams = VillageCoreAPI.otherStreams(page: page)
             return VillageService.shared.request(target: streams)
         }.then { (json: JSON) -> Streams in
-            let groups = json["streams"].arrayValue.compactMap({ Stream(from: $0) })
-            return groups
+            let streams = json["streams"].arrayValue.compactMap({ Stream(from: $0) })
+            return streams
         }
     }
     
-    static func getSubscriptions() -> Promise<Streams> {
+    static func searchOtherStreams(term: String, page: Int)-> Promise<Streams> {
         return firstly {
-            let subscriptions = VillageCoreAPI.subscribedStreams
-            return VillageService.shared.request(target: subscriptions)
+            let streams = VillageCoreAPI.searchOtherStreams(term: term, page: page)
+            return VillageService.shared.request(target: streams)
         }.then { (json: JSON) -> Streams in
-            let groups = json["streams"].arrayValue.compactMap({ Stream(from: $0) })
-            return groups
+            let streams = json["streams"].arrayValue.compactMap({ Stream(from: $0) })
+            return streams
         }
     }
     
-    static func subscribeToStream(streamId: String) -> Promise<Stream> {
+    static func getDetails(of stream: Stream) -> Promise<Stream> {
         return firstly {
-            let streams = VillageCoreAPI.subscribeToStream(streamId: streamId)
-            return VillageService.shared.request(target: streams)
+            let details = VillageCoreAPI.streamDetails(streamId: stream.id)
+            return VillageService.shared.request(target: details)
         }.then { (json: JSON) -> Stream in
-                let stream = try Stream(from: json).orThrow(StreamsServiceError.subscribeToStream)
-                return stream
-        }
-    }
-    
-    static func getStreamDetails(streamId: String) -> Promise<Stream> {
-        return firstly {
-            let streams = VillageCoreAPI.streamDetails(streamId: streamId)
-            return VillageService.shared.request(target: streams)
-        }.then { (json: JSON) -> Stream in
-            let stream = try Stream(from: json).orThrow(StreamsServiceError.streamDetails)
+            let stream = try Stream(from: json).orThrow(StreamsServiceError.unknown)
             return stream
         }
     }
     
-    static func sendMessage(message: Message) -> Promise<Message> {
-        return firstly  {
-            let message = VillageCoreAPI.sendMessage(
-                streamId: message.streamId,
-                messageId: message.id,
-                body: message.text ?? ""
+    static func getMembers(of stream: Stream) -> Promise<People> {
+        return firstly {
+            let members = VillageCoreAPI.streamMembers(streamId: stream.id)
+            return VillageService.shared.request(target: members)
+        }.then { (json: JSON) -> People in
+            let people = json["people"].arrayValue.compactMap({ Person(from: $0) })
+            return people
+        }
+    }
+    
+    static func getMessages(of stream: Stream, page: Int) -> Promise<Messages> {
+        return firstly { () -> Promise<JSON> in
+            let endpoint = VillageCoreAPI.streamMessages(streamId: stream.id, page: page)
+            return VillageService.shared.request(target: endpoint)
+        }.then { (json: JSON) -> Messages in
+            let messages = json["messages"].arrayValue.compactMap({ Message(from: $0) })
+            return messages
+        }
+    }
+    
+    static func getMessages(after message: Message, page: Int) -> Promise<Messages> {
+        return firstly { () -> Promise<JSON> in
+            let endpoint = VillageCoreAPI.streamMessagesStartingAfter(messageId: message.id, streamId: message.streamId, page: page)
+            return VillageService.shared.request(target: endpoint)
+        }.then { (json: JSON) -> Messages in
+            let messages = json["messages"].arrayValue.compactMap({ Message(from: $0) })
+            return messages
+        }
+    }
+    
+    static func createStream(type: Stream.StreamType, name: String, description: String, owner: Person) -> Promise<Stream> {
+        return firstly {
+            let endpoint = VillageCoreAPI.createOrUpdateStream(
+                streamId: "stream-\(owner.id)-\(UUID().uuidString)",
+                type: type.apiType,
+                name: name,
+                description: description,
+                ownerId: String(owner.id)
             )
-            return VillageService.shared.request(target: message)
+            return VillageService.shared.request(target: endpoint)
+        }.then { (json: JSON) -> Stream in
+            let stream = try Stream(from: json).orThrow(StreamsServiceError.unknown)
+            return stream
+        }
+    }
+    
+    static func invitePeople(_ people: People, to stream: Stream) -> Promise<Void> {
+        let invite = VillageCoreAPI.inviteToStream(
+            streamId: stream.id,
+            userIds: people.map({ String($0.id) })
+        )
+        return VillageService.shared.request(target: invite).asVoid()
+    }
+    
+    static func subscribeTo(_ stream: Stream) -> Promise<Void> {
+        let subscribe = VillageCoreAPI.setSubscribed(subscribed: true, streamId: stream.id)
+        return VillageService.shared.request(target: subscribe).asVoid()
+    }
+    
+    static func unsubscribeFrom(_ stream: Stream) -> Promise<Void> {
+        let unsubscribe = VillageCoreAPI.setSubscribed(subscribed: false, streamId: stream.id)
+        return VillageService.shared.request(target: unsubscribe).asVoid()
+    }
+    
+    static func sendMessage(body: String, from person: Person, to stream: Stream) -> Promise<Message> {
+        return firstly  {
+            let newMessage = VillageCoreAPI.sendMessage(
+                streamId: stream.id,
+                messageId: "message-\(person.id)-\(UUID().uuidString)",
+                body: body
+            )
+            return VillageService.shared.request(target: newMessage)
         }.then { (json: JSON) -> Message in
-            let message = try Message(from: json).orThrow(StreamsServiceError.sendMessage)
+            let message = try Message(from: json).orThrow(StreamsServiceError.unknown)
             return message
         }
     }
     
+    static func like(message: Message) -> Promise<Message> {
+        return firstly {
+            let like = VillageCoreAPI.setMessageLiked(isLiked: true, messageId: message.id, streamId: message.streamId)
+            return VillageService.shared.request(target: like).asVoid()
+        }.then {
+            var likedMessage = message
+            likedMessage.isLiked = true
+            return Promise(likedMessage)
+        }
+        
+    }
+    
+    static func dislike(message: Message) -> Promise<Message> {
+        return firstly {
+            let dislike = VillageCoreAPI.setMessageLiked(isLiked: false, messageId: message.id, streamId: message.streamId)
+            return VillageService.shared.request(target: dislike).asVoid()
+        }.then {
+            var dislikedMessage = message
+            dislikedMessage.isLiked = false
+            return Promise(dislikedMessage)
+        }
+    }
+
 }
 
 // MARK: - SwiftyJSON Extensions
@@ -151,46 +238,72 @@ internal extension Stream {
     
     init?(from response: JSON) {
         guard
-            let id = response["streamId"].string  ?? response["id"].string
+            let id = response["id"].string,
+            let name = response["name"].string
         else {
             return nil
         }
         
-        self = Stream.init(
-            //TODO: Harry styles add stuff here
-            deactivated: response["deactivated"].bool,
-            description: response["description"].string,
+        self = Stream(
             id: id,
-            memberCount: response["memberCount"].int,
-            messageCount: response["messageCount"].int,
-            name: response["name"].string,
-            ownerId: response["ownerId"].int,
-            streamType: StreamType.init(apiValue: response["streamType"].string ?? "closed"),
-            closedParties: response["closedParties"].arrayValue.compactMap({ Person(from: $0) })
+            name: name,
+            details: Stream.Details(from: response)
         )
     }
     
 }
 
+internal extension Stream.Details {
+
+    init?(from response: JSON) {
+        let responseType = response["type"].string ?? response["streamType"].string
+        guard
+            let streamType = Stream.StreamType(apiValue: responseType),
+            let ownerId = response["ownerId"].int
+        else {
+            return nil
+        }
+        
+        self = Stream.Details(
+            streamType: streamType,
+            description: response["description"].stringValue,
+            ownerId: String(ownerId),
+            messageCount: response["messageCount"].intValue,
+            memberCount: response["memberCount"].intValue,
+            closedParties: response["closedParties"].arrayValue.compactMap({ Person(from: $0) }),
+            peopleIds: response["peopleIds"].arrayValue.compactMap({ $0.string }),
+            deactivated: response["deactivated"].boolValue
+        )
+    }
+
+}
+
 internal extension Message {
     
     init?(from response: JSON) {
-        guard let id = response["id"].string
+        guard
+            let id = response["id"].string,
+            let author = Person(from: response["person"]),
+            let authorId = response["ownerId"].string,
+            let authorDisplayName = response["ownerDisplayName"].string,
+            let streamId = response["streamId"].string
         else {
-                return nil
+            return nil
         }
         
         self = Message.init(
             id: id,
-            person: Person(from: response["person"]),
-            text: response["text"].stringValue,
-            ownderDisplayName: response["ownerDisplayName"].stringValue,
-            lastUpdated: response["lastUpdated"].stringValue,
-            createdDate: response["createdDate"].stringValue,  
-            streamId: response["streamid"].stringValue,
+            author: author,
+            authorId: authorId,
+            authorDisplayName: authorDisplayName,
+            streamId: streamId,
+            body: response["text"].stringValue,
+            isLiked: response["hasUserLikedMessage"].boolValue,
             likesCount: response["likesCount"].intValue,
-            hasUserLikedMessage: response["hasUserLikedMessage"].boolValue,
+            created: response["createdDate"].stringValue,
+            updated: response["lastUpdated"].stringValue,
             isSystem: response["isSystem"].boolValue
         )
     }
+    
 }
