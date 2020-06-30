5e305f21dbb68c0079434f7065d19ed375fb8e8d
Grayson Hansard <grayson.hansard@willowtreeapps.com>
2020-06-30 16:08:29 -0400

    Add simple test to verify contents of menu bar

--- Clients/DonatosVillage/DonatosVillageUITests/DonatosVillageUITests.swift
+++ Clients/DonatosVillage/DonatosVillageUITests/DonatosVillageUITests.swift
@@ -30,6 +30,25 @@ class DonatosVillageUITests: XCTestCase {
 				XCTAssertTrue(app.webViews.webViews.textViews/*@START_MENU_TOKEN@*/.staticTexts["Checking to see that the ™, the © and the ® all work in a notice."]/*[[".otherElements[\"Content Library Page Preview\"]",".otherElements[\"main\"].staticTexts[\"Checking to see that the ™, the © and the ® all work in a notice.\"]",".staticTexts[\"Checking to see that the ™, the © and the ® all work in a notice.\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.waitForExistence(timeout: 2.0))
 		}
 	}
+	
+	func testMenuBar() throws {
+		Application()
+			.login(with: .Admin, behavior: .ignoreAndContinueIfAlreadyLoggedIn)
+			.openMenuBar()
+			.then {
+				let staticTexts = $0.scrollViews.otherElements.staticTexts
+				XCTAssertTrue(staticTexts["Home"].waitForExistence(timeout: 2.0))
+				XCTAssertTrue(staticTexts["Notices"].exists)
+				XCTAssertTrue(staticTexts["Events"].exists)
+				XCTAssertTrue(staticTexts["News"].exists)
+				XCTAssertTrue(staticTexts["Chats"].exists)
+				XCTAssertTrue(staticTexts["Promise in Actions"].exists)
+				XCTAssertTrue(staticTexts["My Schedule"].exists)
+				XCTAssertTrue(staticTexts["My Groups"].exists)
+				XCTAssertTrue(staticTexts["Other Groups"].exists)
+				XCTAssertTrue(staticTexts["People"].exists)
+		}
+	}
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
