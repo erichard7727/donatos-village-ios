04ac27f4dcf8734b237d851ffc847228d9f80515
Grayson Hansard <grayson.hansard@willowtreeapps.com>
2020-07-01 14:31:30 -0400

    (Temporary) Fix test
    We need to provide a proper identifier or else we'll see this test break again in the future as we add more data or adjust what this test user has access to.

--- Clients/DonatosVillage/DonatosVillageUITests/DonatosVillageUITests.swift
+++ Clients/DonatosVillage/DonatosVillageUITests/DonatosVillageUITests.swift
@@ -25,7 +25,7 @@ class DonatosVillageUITests: XCTestCase {
 		Application()
 			.login(with: .AutomationStoreAssociation, behavior: .ignoreAndContinueIfAlreadyLoggedIn)
 			.then { app in
-				app.scrollViews.containing(.other, identifier:"Vertical scroll bar, 5 pages").children(matching: .other).element(boundBy: 0).children(matching: .other).element(boundBy: 2)/*@START_MENU_TOKEN@*/.staticTexts["View All"]/*[[".buttons[\"View All\"].staticTexts[\"View All\"]",".staticTexts[\"View All\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+				app.scrollViews.containing(.other, identifier:"Vertical scroll bar, 4 pages").children(matching: .other).element(boundBy: 0).children(matching: .other).element(boundBy: 2)/*@START_MENU_TOKEN@*/.staticTexts["View All"]/*[[".buttons[\"View All\"].staticTexts[\"View All\"]",".staticTexts[\"View All\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
 				app.tables/*@START_MENU_TOKEN@*/.staticTexts["New Holiday Pizzas"]/*[[".cells.staticTexts[\"New Holiday Pizzas\"]",".staticTexts[\"New Holiday Pizzas\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
 				XCTAssertTrue(app.webViews.webViews.textViews/*@START_MENU_TOKEN@*/.staticTexts["Checking to see that the ™, the © and the ® all work in a notice."]/*[[".otherElements[\"Content Library Page Preview\"]",".otherElements[\"main\"].staticTexts[\"Checking to see that the ™, the © and the ® all work in a notice.\"]",".staticTexts[\"Checking to see that the ™, the © and the ® all work in a notice.\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.waitForExistence(timeout: 2.0))
 		}
