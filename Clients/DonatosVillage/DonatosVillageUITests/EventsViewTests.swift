d1676ee3188cab76b3f7ab2f21cf4a8c6d756f86
Grayson Hansard <grayson.hansard@willowtreeapps.com>
2020-07-24 16:44:15 -0400

    Add tests for setting Event statuses

--- Clients/DonatosVillage/DonatosVillageUITests/EventsViewTests.swift
+++ Clients/DonatosVillage/DonatosVillageUITests/EventsViewTests.swift
@@ -1,5 +1,19 @@
 import XCTest
 
+private func testSettingStatus(app: XCUIApplication, buttonTitle: String, expectedLabel: String, expectedImageIdentifier: String) {
+	XCTAssert(app.navigationBars.staticTexts["Events"].exists)
+	app.tables.cells.firstMatch.tap()
+	
+	let goingButton = app.buttons[buttonTitle]
+	XCTAssert(goingButton.waitForExistence(timeout: 1.0))
+	goingButton.tap()
+	
+	app.navigationBars.buttons.firstMatch.tap()
+	
+	XCTAssertEqual(expectedLabel, app.tables.cells.firstMatch.staticTexts.firstMatch.label)
+	XCTAssertEqual(expectedImageIdentifier, app.tables.firstMatch.cells.firstMatch.images.firstMatch.identifier)
+}
+
 class EventsViewTests: XCTestCase {
 	override func setUpWithError() throws {
 		continueAfterFailure = false
@@ -26,6 +40,17 @@ class EventsViewTests: XCTestCase {
 			XCTAssert(buttons["Going"].waitForExistence(timeout: 1.0))
 			XCTAssert(buttons["Interested"].waitForExistence(timeout: 1.0))
 			XCTAssert(buttons["Not Going"].waitForExistence(timeout: 1.0))
+		}
+	}
+	
+	func testSettingEventStatuses() throws {
+		Application()
+		.login(with: .AutomationStoreAssociation)
+		.openEventsMenu()
+		.then { app in
+			testSettingStatus(app: app, buttonTitle: "Going", expectedLabel: "GOING", expectedImageIdentifier: "notice-needs-action")
+			testSettingStatus(app: app, buttonTitle: "Interested", expectedLabel: "INTERESTED", expectedImageIdentifier: "notice-interested")
+			testSettingStatus(app: app, buttonTitle: "Not Going", expectedLabel: "NOT GOING", expectedImageIdentifier: "notice-not-going")
 		}
 	}
 }
