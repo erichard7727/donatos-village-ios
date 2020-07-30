80b39a766f7a4f8d5172fe32a1efb0c493a75991
Scott Harman <scott.harman@willowtreeapps.com>
2020-07-30 08:35:26 -0400

    Adds documentation to file

--- Product/Core/VillageCore/Source/Models/Message.swift
+++ Product/Core/VillageCore/Source/Models/Message.swift
@@ -11,10 +11,10 @@ import Foundation
 /// An alias name that represents a list of messages
 public typealias Messages = [Message]
 
-
+///A discrete text communication sent to a recipient or a group of receipients.
 public struct Message {
     
-    // MARK: - Constants
+    // MARK: - Constant Stored Properties
     
     public let id: String
     public let author: Person
@@ -29,6 +29,22 @@ public struct Message {
     public let isSystem: Bool
     public let attachment: Attachment?
     
+    // MARK: - Intialization
+    
+    /// Initialize a new Message instance
+    /// - Parameters:
+    ///   - id: The identifier of the message instance
+    ///   - author: The person who created the message
+    ///   - authorId: The unique identifier associated with the autor of the message
+    ///   - authorDisplayName: The name visable to the people receiving the message
+    ///   - streamId: The unique identifier associated with a particular stream
+    ///   - body: The content of the message
+    ///   - isLiked: An action that can be made by the recipients as a quick way to show approval of the messsage
+    ///   - likesCount: The total of likes a message has received by all recipients
+    ///   - created: The orignal date the message instance was created
+    ///   - updated: The last date the message was updated by the author or recipients
+    ///   - isSystem: An indicator of whether the message was created by the system rather than an author
+    ///   - attachment: An optional media file that can be sent along with a message
     public init(
         id: String,
         author: Person,
@@ -60,6 +76,7 @@ public struct Message {
 
 public extension Message {
     
+    /// A media file that is sent along with a message such as an image, meme or video
     struct Attachment {
         
         // MARK: - Constants
@@ -71,6 +88,16 @@ public extension Message {
         public let height: Double
         public let url: URL
         
+        // MARK: - Intialization
+        
+        /// Initialize a new Attachment instance
+        /// - Parameters:
+        ///   - content: The actual media file sent that represents the attachment instance
+        ///   - type: The type of file sent with a message such an image, meme or video.
+        ///   - title: The name that describes the attachment
+        ///   - width: An attribute of a rendered media file that represents the measurment from side to side
+        ///   - height: An attribute of a rendered media file that represents the measurment from top to bottom
+        ///   - url: The internet address of the media file being attached to a message
         public init(
             content: String,
             type: String,
