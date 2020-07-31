95c76fee9da9d4f82fc2b08573c5b9fb1a3f33f2
Nikola Angelkovik <nikola.angelkovik@willowtreeapps.com>
2020-07-31 11:49:02 +0200

    Add test to verify that the save button exists

--- Clients/DonatosVillage/DonatosVillageUITests/SettingsViewTests.swift
+++ Clients/DonatosVillage/DonatosVillageUITests/SettingsViewTests.swift
@@ -23,4 +23,15 @@ class SettingsViewTests: XCTestCase {
                 XCTAssert(app.navigationBars.matching(identifier: "Edit Settings").count != 0, "Cannot reach Edit Settings screen")
         }
     }
+    
+    func testIfTheSaveButtonExists() throws {
+        Application()
+            .login(with: .automationStoreAssociation)
+            .openMenuBar()
+            .then { app in
+                app.otherElements["current_user_container"].tap()
+                XCTAssert(app.navigationBars.matching(identifier: "Edit Settings").count != 0, "Cannot reach Edit Settings screen")
+                XCTAssertTrue(app.navigationBars["Edit Settings"].buttons["Save"].exists, "The Save button should be present in the Edit Settings controller")
+        }
+    }
 }
