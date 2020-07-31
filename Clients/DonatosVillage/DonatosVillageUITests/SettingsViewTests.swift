67582be166473a2944b257c687bf982cffcd4710
Nikola Angelkovik <nikola.angelkovik@willowtreeapps.com>
2020-07-31 11:48:07 +0200

    Add test for opening the Settings screen

---
+++ Clients/DonatosVillage/DonatosVillageUITests/SettingsViewTests.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsViewTests.swift
+//  DonatosVillageUITests
+//
+//  Created by Nikola Angelkovik on 7/31/20.
+//  Copyright © 2020 Donatos. All rights reserved.
+//
+
+import XCTest
+
+class SettingsViewTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+    
+    func testOpeningSettingsScreen() throws {
+        Application()
+            .login(with: .automationStoreAssociation)
+            .openMenuBar()
+            .then { app in
+                app.otherElements["current_user_container"].tap()
+                XCTAssert(app.navigationBars.matching(identifier: "Edit Settings").count != 0, "Cannot reach Edit Settings screen")
+        }
+    }
+}
