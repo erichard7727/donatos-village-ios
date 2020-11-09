22fce5aadfd23ba2b3137a0d0f80dce2a80ded42
Sasho Jadrovski <sasho.jadrovski@willowtreeapps.com>
2020-11-09 16:03:07 +0100

    Use the login URL from the MySchedule utility class, add a title to the nav bar, implement back or menu button and fix the nav bar style

--- Product/Core/VillageCoreUI/Source/Screens/MySchedule/MyScheduleViewController.swift
+++ Product/Core/VillageCoreUI/Source/Screens/MySchedule/MyScheduleViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import WebKit
 import VillageCore
 
-class MyScheduleViewController: UIViewController {
+class MyScheduleViewController: UIViewController, NavBarDisplayable {
 	public static func create() -> MyScheduleViewController {
 		let storyboard = UIStoryboard(name: "MySchedule", bundle: Constants.bundle)
         return storyboard.instantiateInitialViewController() as! MyScheduleViewController
@@ -18,15 +18,23 @@ class MyScheduleViewController: UIViewController {
 			loadInitialRequest()
 		}
 	}
-
-	override func viewDidLoad() {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
+        title = NSLocalizedString("My Schedule", comment: "")
+        addBehaviors([
+            LeftBarButtonBehavior(showing: .menuOrBack)    
+        ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setOpaqueNavbarAppearance(for: navigationItem, in: navigationController)
     }
 
 	public func load(credentials: MySchedule.Credentials) {
-		let url = URL(string: "https://schedule.donatos.com/schedule/pepptalk/getloginUser")!
 		let body = "email=\(credentials.encryptedEmail)&securityKey=\(credentials.securityKey)"
-		var request = try! URLRequest(url: url, method: .post)
+        var request = try! URLRequest(url: MySchedule.loginUrl, method: .post)
 		request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
 		request.httpBody = body.data(using: .utf8)
 		initialRequest = request
